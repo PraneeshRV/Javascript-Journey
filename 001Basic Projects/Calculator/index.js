cc60bc4a99690a49ca2a20612c2482ad7919a0bf
Praneesh R V <praneeshrv404@gmail.com>
2024-10-07 18:15:22 +0530

    Calculator program

---
+++ 001Basic Projects/Calculator/index.js
@@ -0,0 +1,20 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
