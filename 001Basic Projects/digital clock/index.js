c4a0e007de371dae97eeefd236c747b56f33f60d
Praneesh R V <praneeshrv404@gmail.com>
2024-10-07 17:16:33 +0530

    digital clock

---
+++ 001Basic Projects/digital clock/index.js
@@ -0,0 +1,17 @@
+// DIGITAL CLOCK PROGRAM
+
+function updateClock(){
+
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById("clock").textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
