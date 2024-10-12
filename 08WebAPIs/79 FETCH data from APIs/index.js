22a6724563c39665ab8971f11c05e001bbe476fd
Praneesh R V <praneeshrv404@gmail.com>
2024-10-12 20:11:44 +0530

    Day 8

---
+++ 08WebAPIs/79 FETCH data from APIs/index.js
@@ -0,0 +1,29 @@
+// fetch = Function used for making HTTP requests to fetch resources.
+//         (JSON style data, images, files)
+//         Simplifies asynchronous data fetching in JavaScript and
+//         used for interacting with APIs to retrieve and send
+//         data asynchronously over the web.
+//         fetch(url, {options})
+
+async function fetchData(){
+
+    try{
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error){
+        console.error(error);
+    }
+}
