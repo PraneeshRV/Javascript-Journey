3df11df6b8982a33745f64144f9564d86714ece1
Praneesh R V <praneeshrv404@gmail.com>
2024-10-12 19:43:58 +0530

    Day 8

---
+++ 05Async-notes/75 Js Promises/index.js
@@ -0,0 +1,65 @@
+// Promise = An Object that manages asynchronous operations.
+//                    Wrap a Promise Object around {asynchronous code}
+//                    "I promise to return a value"
+//                    PENDING -> RESOLVED or REJECTED
+//                   new Promise((resolve, reject) => {asynchronous code})
+
+// DO THESE CHORES IN ORDER
+
+// 1. WALK THE DOG
+// 2. CLEAN THE KITCHEN
+// 3. TAKE OUT THE TRASH
+
+function walkDog(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const dogWalked = false;
+
+            if(dogWalked){
+                resolve("You walk the dog 🐕");
+            }
+            else{
+                reject("You DIDN'T walk the dog");
+            }
+        }, 1500);
+    });
+}
+
+function cleanKitchen(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+            const kitchenCleaned = true;
+
+            if(kitchenCleaned){
+                resolve("You clean the kitchen 🧹");
+            }
+            else{
+                reject("You DIDN'T clean the kitchen");
+            }
+        }, 2500);
+    });
+}
+
+function takeOutTrash(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const trashTakenOut = true;
+
+            if(trashTakenOut){
+                resolve("You take out the trash ♻");
+            }
+            else{
+                reject("You DIDN'T take out the trash");
+            }
+
+        }, 500);
+    });
+}
+
+walkDog().then(value => {console.log(value); return cleanKitchen()})
+                  .then(value => {console.log(value); return takeOutTrash()})
+                  .then(value => {console.log(value); console.log("You finished all the chores!")})
+                  .catch(error => console.error(error));
---
+++ 05Async-notes/76 Async&Await/index.js
@@ -0,0 +1,76 @@
+// Async/Await = Async = makes a function return a promise
+//                           Await = makes an async function wait for a promise
+
+// Allows you write asynchronous code in a synchronous manner
+// Async doesn't have resolve or reject set up as parameters
+// Everything after Await is placed in an event queue
+
+function walkDog(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const dogWalked = true;
+
+            if(dogWalked){
+                resolve("You walk the dog 🐕");
+            }
+            else{
+                reject("You DIDN'T walk the dog");
+            }
+        }, 1500);
+    });
+}
+
+function cleanKitchen(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+            const kitchenCleaned = true;
+
+            if(kitchenCleaned){
+                resolve("You clean the kitchen 🧹");
+            }
+            else{
+                reject("You DIDN'T clean the kitchen");
+            }
+        }, 2500);
+    });
+}
+
+function takeOutTrash(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const trashTakenOut = true;
+
+            if(trashTakenOut){
+                resolve("You take out the trash ♻");
+            }
+            else{
+                reject("You DIDN'T take out the trash");
+            }
+
+        }, 500);
+    });
+}
+
+async function doChores(){
+
+    try{
+        const walkDogResult = await walkDog();
+        console.log(walkDogResult);
+    
+        const cleanKitchenResult = await cleanKitchen();
+        console.log(cleanKitchenResult);
+    
+        const takeOutTrashResult = await takeOutTrash();
+        console.log(takeOutTrashResult);
+        
+        console.log("You finsihed all the chores!");
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+doChores();
